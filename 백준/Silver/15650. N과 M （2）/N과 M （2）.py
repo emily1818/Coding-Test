589cfa4530746a13d7f72c84fa11607143fd811b
emily1818 <jh991219@naver.com>
2025-02-24 20:31:06 +0900

    edit

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,15 +1,17 @@
-n,m = list(map(int,input().split()))
-s = []
-def dfs(start):
-    if len(s)==m:
-        print(' '.join(map(str,s)))
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+res = []
+
+def dfs(x):
+    if len(res) == m:
+        print(' '.join(map(str, res)))
         return
-    
-    for i in range(start,n+1):
-        if i not in s:
-            s.append(i)
-            dfs(i+1)
-            s.pop()
+
+    for i in range(x, n+1):
+        if i not in res:
+            res.append(i)
+            dfs(i + 1)
+            res.pop()
 dfs(1)
- 
-    
