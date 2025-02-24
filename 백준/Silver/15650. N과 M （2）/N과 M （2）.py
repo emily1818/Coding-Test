326e2c6e34f5e9c469bcbc92a6fdece4408e75dc
emily1818 <jh991219@naver.com>
2025-02-24 20:47:55 +0900

    master

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -15,18 +15,21 @@ def dfs(x):
             dfs(i + 1)
             res.pop()
 dfs(1)
-#push 확인
-n,m = list(map(int,input().split()))
-s = []
-def dfs(start):
-    if len(s)==m:
-        print(' '.join(map(str,s)))
-        return
+
+
+
+# #push 확인
+# n,m = list(map(int,input().split()))
+# s = []
+# def dfs(start):
+#     if len(s)==m:
+#         print(' '.join(map(str,s)))
+#         return
     
-    for i in range(start,n+1):
-        if i not in s:
-            s.append(i)
-            dfs(i+1)
-            s.pop()
-dfs(1)
+#     for i in range(start,n+1):
+#         if i not in s:
+#             s.append(i)
+#             dfs(i+1)
+#             s.pop()
+# dfs(1)
  
