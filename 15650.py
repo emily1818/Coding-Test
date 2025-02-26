38047a194d5b295713a414925067ddb2a66ad9aa
emily1818 <jh991219@naver.com>
2025-02-26 14:44:06 +0900

    9095 edit

--- 15650.py
+++ 15650.py
@@ -1,44 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-
-not_heard = []
-not_seen = []
-cnt = 0 
-not_heard_seen = []
-
-for i in range(N) :
-    name = input()
-    # print(name)
-    not_heard.append(name)
-
-for i in range(M) : 
-    name = input()
-    not_seen.append(name)
-
-#각 arr를 set으로 풀어주고 &연산자를 통해 교집합 부분만 빼서 list로 만들고 출력
-not_heard_seen = list(set(not_heard) & set(not_seen))
-#시간 초과 남..;;
-# for name in not_heard : 
-#     if name in not_seen :
-#         cnt +=1 
-#         not_heard_seen.append(name)
-
-not_heard_seen.sort()
-
-# print(not_heard[0])
-print(len(not_heard_seen))
-
-# for name in not_heard_seen :
-#     print(name)
-
-print("".join(not_heard_seen), end = "")
-
-###########################
-#set
-# 순서가 없고 중복을 허용하지 않는 컬렉션 
-#1. 중복 제거 
-#2. 빠른 멤버십 검사
-#3. 수학적 집합 연산 : 합집합, 교집합, 차집합 등 집합 연산 지원
-# A|B, A&B A-B
+N = map(int, input())
+arr = list(map(int, input().split()))
+ans = 0
+waiting_time = 0 
+#시간순대로 정렬
+arr.sort()
+for idx, i in enumerate(arr) :
+    waiting_time = 0 
+    for j in arr[:idx+1] :
+        waiting_time += j
+    ans += waiting_time
+print(ans)
--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,16 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-t = int(input())
+T = int(input())
 
-cnt = [0] * 11
-cnt[1] = 1
-cnt[2] = 2
-cnt[3] = 4
+arr = [0] * 11
+arr[1] = 1
+arr[2] = 2
+arr[3] = 4
+for i in range(4, 11) :
+    arr[i] = arr[i-3] + arr[i-2] + arr[i-1]
+    
+for _ in range(T) :
+    num = int(input())
+    print(arr[num])
 
-for i in range(4, 11):
-    cnt[i] = cnt[i-3] + cnt[i-2] + cnt[i-1]
 
-for _ in range(t):
-    n = int(input())
-    print(cnt[n])
+
+
+
+# t = int(input())
+
+# cnt = [0] * 11
+# cnt[1] = 1
+# cnt[2] = 2
+# cnt[3] = 4
+
+# for i in range(4, 11):
+#     cnt[i] = cnt[i-3] + cnt[i-2] + cnt[i-1]
+
+# for _ in range(t):
+#     n = int(input())
+#     print(cnt[n])
