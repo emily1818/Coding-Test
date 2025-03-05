308271271ec2d6c82de16f9ea5a611c2f67a4276
emily1818 <112855303+emily1818@users.noreply.github.com>
2025-03-05 18:21:32 +0900

    [Silver II] Title: 유기농 배추, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,43 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input()) #테스트케이스의 개수
+T = int(input())
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
 
-def BFS(x,y):           
-    queue = [(x,y)]
-    matrix[x][y] = 0 # 방문처리
+def BFS(x, y) :
+    queue = [(x, y)]
+    arr[x][y] = 0 
+    while queue :
+        x, y = queue.pop(0)
 
-    while queue:
-        x,y = queue.pop(0)
-
-        for i in range(4):
+        for i in range(4) :
             nx = x + dx[i]
             ny = y + dy[i]
-
+            
             if nx < 0 or nx >= M or ny < 0 or ny >= N:
                 continue
 
-            if matrix[nx][ny] == 1 :
-                queue.append((nx,ny))
-                matrix[nx][ny] = 0
+            if arr[nx][ny] == 1:
+                arr[nx][ny] = 0 
+                queue.append([nx, ny])
 
-# 행렬만들기
-for i in range(T):
-    M, N, K = map(int,input().split())
-    matrix = [[0]*(N) for _ in range(M)]
-    cnt = 0
 
-    for j in range(K):
-        x,y = map(int, input().split())
-        matrix[x][y] = 1
+for _ in range(T) :
+    cnt =0 
+    M, N, K = map(int, input().split())
+    arr = [[0] * (N) for _ in range(M)]
 
-    for a in range(M):
-        for b in range(N):
-            if matrix[a][b] == 1:
-                BFS(a,b)
+    #행렬 만들기
+    for _ in range(K) :
+        x, y = map(int, input().split())
+        arr[x][y] = 1
+
+    for x in range(M) :
+        for y in range(N) :
+            if arr[x][y] == 1:
+                BFS(x, y)
                 cnt += 1
-
     print(cnt)
