3636823898147a8e97c8c8211199a66463d340d8
emily1818 <112855303+emily1818@users.noreply.github.com>
2025-04-02 18:12:27 +0900

    [Gold IV] Title: 부분합, Time: 96 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,22 +1,36 @@
-n, s = map(int, input().strip().split())
-nums = list(map(int, input().strip().split()))
+## 1806
+import sys
+input = sys.stdin.readline
 
-left, right = 0,0
-sum = 0
-min_length=1e9
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
 
-while True:
-    if sum >= s:
-        min_length=min(min_length,right-left)
-        sum -= nums[left]
-        left +=1
-    elif right == n:
+start = 0 
+end = 0
+result = 1000000000
+sum_num =0 
+
+
+while True :
+    if sum_num >= S :
+        temp = end - start
+        if result > temp :
+            result = temp
+        sum_num -= arr[start]
+        start += 1
+
+    elif end == N :
         break
-    else:
-        sum += nums[right]
-        right+=1
+    else :
+        sum_num += arr[end]
+        end += 1
 
-if min_length == 1e9:
+if result == 1000000000:
     print(0)
-else:
-    print(min_length)
+else : 
+
+    print(result)
+        
+
+
+
