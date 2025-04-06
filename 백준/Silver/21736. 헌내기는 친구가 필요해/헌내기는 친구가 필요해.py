20863d5fac5dd4945730212c8459d6d01c95ccb7
emily1818 <112855303+emily1818@users.noreply.github.com>
2025-04-06 18:15:46 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 860 ms, Memory: 37708 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,44 +1,40 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().strip().split())
 
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
-cnt = 0 
+cnt = 0
 
 arr = [list(input().strip()) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
 
 def bfs(x, y):
     global cnt
-    queue = deque()
-    queue.append((x, y))
-    visited[x][y] = True
+    queue = [(x, y)]
+    arr[x][y] = "X"  # 방문 처리
 
     while queue:
-        x, y = queue.popleft()
+        x, y = queue.pop(0)
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+            if 0 <= nx < n and 0 <= ny < m:
                 if arr[nx][ny] == "O":
-                    visited[nx][ny] = True
+                    arr[nx][ny] = "X"
                     queue.append((nx, ny))
                 elif arr[nx][ny] == "P":
-                    visited[nx][ny] = True
+                    arr[nx][ny] = "X"
                     queue.append((nx, ny))
                     cnt += 1
 
 # 시작점 찾기
-for i in range(n): 
+for i in range(n):
     for j in range(m):
         if arr[i][j] == "I":
-            start_x, start_y = i, j
-            bfs(start_x, start_y)
+            bfs(i, j)
 
 if cnt:
     print(cnt)
