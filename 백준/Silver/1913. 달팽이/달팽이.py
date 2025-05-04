4985c8b08450cb389ae5b82f47055e327a59918b
emily1818 <112855303+emily1818@users.noreply.github.com>
2025-05-04 17:53:45 +0900

    [Silver III] Title: 달팽이, Time: 684 ms, Memory: 70928 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -31,3 +31,54 @@ while cur <= n * n:
 for row in arr:
     print(' '.join(map(str, row)))
 print(target_x + 1, target_y + 1)
+
+# import sys 
+
+# input = sys.stdin.readline
+
+# n = int(input())
+# num = int(input())
+# arr = [[0]*(n) for _ in range(n)]
+# result_x  =0 
+# result_y = 0 
+# if n % 2 == 0 :
+#     x = n//2 
+#     y = n //2 -1
+
+# else : 
+#     x = n//2
+#     y = n //2 
+
+# dx = [-1, 0, 1, 0]
+# dy = [0, 1, 0, -1]
+# idx = 0 
+# arr[x][y] = 1
+
+# for i in range(2, n * n + 1) :    
+#     nx = x + dx[idx]
+#     ny = y + dy[idx]
+    
+#     if  arr[nx][ny] != 0 :
+#         idx = idx -1
+#         if idx == -1 :
+#             idx = 3 
+#         x = x + dx[idx]
+#         y = y + dy[idx]
+#         arr[x][y] = i 
+
+#     else :
+#         arr[nx][ny] = i
+#         x = nx
+#         y = ny
+
+#     idx = idx +1 
+#     if idx ==4 :
+#         idx = 0 
+#     if i == num : 
+#         result_x = x
+#         result_y = y
+
+
+# for line in arr : 
+#     print(" ".join(map(str, line)))
+# print(result_x + 1, result_y + 1)
