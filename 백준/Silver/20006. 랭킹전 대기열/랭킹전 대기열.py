22423279756512078a2b1d621d9d737326a3d0b0
emily1818 <112855303+emily1818@users.noreply.github.com>
2025-05-04 15:30:09 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -1,29 +1,35 @@
-import sys
+import sys 
+
 input = sys.stdin.readline
 
 p, m = map(int, input().split())
 rooms = []
 
-for _ in range(p):
-    level, name = input().split()
-    level = int(level)
+for _ in range(p) :
 
-    placed = False
-    for room in rooms:
-        room_level = room[0][0]  # 기준 레벨
-        if len(room) < m and room_level - 10 <= level <= room_level + 10:
-            room.append((level, name))
-            placed = True
-            break
+    i, n = map(str, input().split())
+    i = int(i)
 
-    if not placed:
-        rooms.append([(level, name)])  # 새 방 생성
+    placed = False  
+    for room in rooms : 
+        room_level = room[0][0]
+        
+        if len(room) < m  and int(i) <= room_level+10 and int(i)>= room_level-10 :
+            room.append((i, n))
+            placed = True 
+            break 
+        
+    if placed == False : 
+        rooms.append([(i, n)])
 
-# 출력
-for room in rooms:
-    if len(room) == m:
+for room in rooms :
+    if len(room) == m :
         print("Started!")
-    else:
+       
+    else :
         print("Waiting!")
+
     for player in sorted(room, key=lambda x: x[1]):
         print(player[0], player[1])
+
+
