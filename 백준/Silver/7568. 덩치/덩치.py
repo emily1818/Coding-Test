a16b5312f669994d016d52aa9fea64f0399d664b
emily1818 <112855303+emily1818@users.noreply.github.com>
2025-05-05 17:43:44 +0900

    [Silver V] Title: 덩치, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+people = []
+
+for _ in range(n):
+    weight, height = map(int, input().split())
+    people.append((weight, height))
+
+result = []
+
+for i in range(n):
+    rank = 1
+    for j in range(n):
+        if i == j:
+            continue
+        if people[j][0] > people[i][0] and people[j][1] > people[i][1]:
+            rank += 1
+    result.append(rank)
+
+print(' '.join(map(str, result)))
